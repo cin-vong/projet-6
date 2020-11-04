a188ced734e27324496137710902319805ce3934
cin-vong <61393238+cin-vong@users.noreply.github.com>
2020-11-04 20:48:14 +0100

    projet-6-02

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path')
@@ -8,7 +9,6 @@ const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/users')
 
 const app = express();
-
 
 app.use(helmet());
 
@@ -20,8 +20,7 @@ app.use((req, res, next) => {
   });
 
   app.use(bodyParser.json());
- 
-
+  
   mongoose.connect('mongodb+srv://so-pekocko:NgBL0rDrFf7jE4rO@cluster0.z8zqc.mongodb.net/so-pekocko-sauces?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -30,7 +29,18 @@ app.use((req, res, next) => {
 
   mongoose.set('useCreateIndex', true);
 
+//debug mod of mongoose//
+mongoose.set('debug', true);
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
   app.use('/images', express.static(path.join(__dirname, 'images')))
+
+  app.use(limiter);
 
   app.use('/api/sauces', saucesRoutes);
   app.use('/api/auth', userRoutes);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const passwordValidator = require('password-validator');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
@@ -8,28 +7,5 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.plugin(uniqueValidator);
-
-// Create a schema
-const schema = new passwordValidator();
- 
-// Add properties to it
-schema
-.is().min(8)                                    // Minimum length 8
-.is().max(20)                                  // Maximum length 20
-.has().uppercase()                              // Must have uppercase letters
-.has().lowercase()                              // Must have lowercase letters
-.has().digits(2)                                // Must have at least 2 digits
-.has().not().spaces()                           // Should not have spaces
-.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
- 
-// Validate against a password string
-console.log(schema.validate('validPASS123'));
-// => true
-console.log(schema.validate('invalidPASS'));
-// => false
- 
-// Get a full list of rules which failed
-console.log(schema.validate('joke', { list: true }));
-// => [ 'min', 'uppercase', 'digits' ]
 
 module.exports = mongoose.model('User', userSchema);
